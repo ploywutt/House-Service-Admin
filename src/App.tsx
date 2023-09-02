f5d5727b9fd67cb600639706b8aff55d3cab392b
ploywutt <ploy.wutt@gmail.com>
2023-09-02 20:41:23 +0700

    feat: setup shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button className="bg-blue-500">Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
