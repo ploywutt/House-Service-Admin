f7c16a3c9229d7d80bad0b23b4daebca910933b4
Guer <maythee.srianut@gmail.com>
2023-09-04 16:45:40 +0700

    feat: setting font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Category from "./pages/Category";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* ลบตัวอย่างออกแล้วใส่เฉพาะ Route ที่ทำนะคับ เพื่อป้องกัน conflict คับ */}
-        {/* <Route path="/posts/:id" element={<PostDetail />} /> This is ตัวอย่างคับ*/}
+        <Route path="/categories" element={<Category />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Category.tsx
@@ -0,0 +1,10 @@
+import AdminSidebar from "@/components/AdminSidebar/AdminSidebar";
+
+export default function Category() {
+  return (
+    <div className="flex flex-row">
+      <AdminSidebar />
+      <h1>Categories</h1>
+    </div>
+  );
+}
