dac068c7ef1e6a8d08de6b11ccc7956cf8d9cb9e
Guer <maythee.srianut@gmail.com>
2023-09-04 16:45:40 +0700

    rebase: solve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import "./App.css";
 import Login from "./pages/Login";
 import Category from "./pages/Category";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Category from "./pages/Category";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* ลบตัวอย่างออกแล้วใส่เฉพาะ Route ที่ทำนะคับ เพื่อป้องกัน conflict คับ */}
+<<<<<<< HEAD
         <Route path="/" element={<Login />} />
+=======
+>>>>>>> f7c16a3 (feat: setting font sizes)
         <Route path="/categories" element={<Category />} />
       </Routes>
     </Router>
--- src/components/AdminSidebar/AdminSidebar.tsx
+++ src/components/AdminSidebar/AdminSidebar.tsx
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 import { useNavigate } from "react-router-dom";
+=======
+>>>>>>> f7c16a3 (feat: setting font sizes)
 import CategoryIcon from "../../assets/icon/category.svg";
 import HomeServiceIcon from "../../assets/icon/homeservices.png";
 import LogoutIcon from "../../assets/icon/logout.svg";
@@ -6,6 +9,7 @@ import PromotionIcon from "../../assets/icon/promotion.svg";
 import ServiceIcon from "../../assets/icon/service.svg";
 
 export default function AdminSidebar() {
+<<<<<<< HEAD
   const navigate = useNavigate();
 
   // async function signOutUser() {
@@ -13,10 +17,13 @@ export default function AdminSidebar() {
   //   navigate("/");
   // }
 
+=======
+>>>>>>> f7c16a3 (feat: setting font sizes)
   return (
     <div id="container" className="w-[240px] h-screen bg-blue-950">
       <div
         id="logo-bg"
+<<<<<<< HEAD
         className="bg-blue-100 mb-7 rounded-xl flex flex-row items-center py-2 px-2.5 gap-1 mx-6 mt-6"
       >
         <img src={HomeServiceIcon} alt="HomeServices Icon" id="logo" />
@@ -41,6 +48,32 @@ export default function AdminSidebar() {
           <li className="menu">
             <img src={LogoutIcon} alt="Logout Icon" />
             <h5>ออกจากระบบ</h5>
+=======
+        className="bg-blue-100 mx-6 mt-6 mb-7 flex flex-row items-center gap-2.5"
+      >
+        <img src={HomeServiceIcon} alt="HomeServices Icon" id="logo" />
+        <p>HomeServices</p>
+      </div>
+      <div id="menu-list-container">
+        <ul id="menu-list" className="text-white">
+          <div id="menu-list">
+            <li className="menu-list">
+              <img src={CategoryIcon} alt="Category Icon" />
+              <p>หมวดหมู่</p>
+            </li>
+            <li className="menu-list">
+              <img src={ServiceIcon} alt="Service Icon" />
+              <p>บริการ</p>
+            </li>
+            <li className="menu-list">
+              <img src={PromotionIcon} alt="Promotion Icon" />
+              <p>Promotion Code</p>
+            </li>
+          </div>
+          <li className="menu-list">
+            <img src={LogoutIcon} alt="Logout Icon" />
+            <p>ออกจากระบบ</p>
+>>>>>>> f7c16a3 (feat: setting font sizes)
           </li>
         </ul>
       </div>
