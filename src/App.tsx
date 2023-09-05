bb642bf13fb4415b0839c9601fd5d055b95c2282
Guer <maythee.srianut@gmail.com>
2023-09-05 17:05:39 +0700

    rebase: delete duplicate code

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,13 @@ import "./App.css";
 import Login from "./pages/Login";
 import Category from "./pages/Category";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Category from "./pages/Category";
 
 function App() {
   return (
     <Router>
       <Routes>
         {/* ลบตัวอย่างออกแล้วใส่เฉพาะ Route ที่ทำนะคับ เพื่อป้องกัน conflict คับ */}
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Route path="/" element={<Login />} />
-=======
->>>>>>> f7c16a3 (feat: setting font sizes)
-=======
-        <Route path="/" element={<Login />} />
->>>>>>> 91ffba9f62b9a46305472b27ea0b29db191c7698
         <Route path="/categories" element={<Category />} />
       </Routes>
     </Router>
