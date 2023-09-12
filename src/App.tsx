86da917eb2a6f2fb657d8129a0ab250584dc4c45
BlackCat <neko.sword@gmail.com>
2023-09-12 20:46:47 +0700

    add: feature table dnd in category page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Login from "./pages/Login";
-import Category from "./pages/Category";
+import CategorySearch from "./pages/CategorySearch";
 import Services from "./pages/Services";
 import Promotion from "./pages/Promotion";
 import NotFoundPage from './pages/NotFoundPage'
+import CategoryAdd from "./pages/CategoryAdd";
+import CategoryEdit from "./pages/CategoryEdit";
+import CategoryDetail from "./pages/CategoryDetail";
 
 function App() {
   return (
@@ -14,11 +17,16 @@ function App() {
         {/* ลบตัวอย่างออกแล้วใส่เฉพาะ Route ที่ทำนะคับ เพื่อป้องกัน conflict คับ */}
         <Route path="/" element={<Login />} />
         <Route element={<Layout />}>
-          <Route path="/categories/*" element={<Category />} />
+          <Route path="/categories/*">
+            <Route path="" element={<CategorySearch />} />
+            <Route path="add" element={<CategoryAdd />} />
+            <Route path="edit" element={<CategoryEdit />} />
+            <Route path="detail" element={<CategoryDetail />} />
+          </Route>
           <Route path="/services/*" element={<Services />} />
           <Route path="/promotions/*" element={<Promotion />} />
-          <Route path="*" element={<NotFoundPage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/CategoryAdd.tsx
@@ -0,0 +1,13 @@
+import { Topbar_add } from "@/components/Topbar/Topbar"
+
+function CategoryAdd() {
+	
+	return (
+		<div className="w-full">
+      <Topbar_add title='หมวดหมู่' />
+			<div>CategoryAdd</div>
+    </div>
+	)
+}
+
+export default CategoryAdd
