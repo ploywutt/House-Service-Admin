c5d14088443ebcbb52b13d87cb053b378a59f6c6
themeyv <themeyv@gmail.com>
2023-09-26 22:58:40 +0700

    feat: add axios again

--- src/pages/EmployeeDetail.tsx
+++ src/pages/EmployeeDetail.tsx
@@ -51,15 +51,15 @@ function EmployeeDetail() {
       {/* {loading ? <h1>Loading ...</h1> : null} */}
       <div className="mx-auto w-[90%] max-w-[1440px] mt-10 border rounded-lg">
         <table className="table-auto w-full">
-          <thead className="h-10 bg-gray-200 border-b">
-            <tr className="text-left text-gray-700 .p1">
-              <th className="w-full px-6 py-2.5 rounded-tl-lg flex justify-evenly">
-                <th className="text-center w-60 px-6">รหัส</th>
-                <th className="text-center w-60 px-6">เวลาทำงาน</th>
-                <th className="text-center w-60 rounded-tr-lg">สถานะ</th>
-              </th>
-            </tr>
-          </thead>
+          {/* <thead className=""> */}
+          <tr className="text-left text-gray-700 p1 h-10 bg-gray-200 border-b">
+            <th className="w-full px-6 py-2.5 rounded-t-lg">
+              <td className="text-center w-60 px-6">รหัส</td>
+              <td className="text-center w-60 px-6">เวลาทำงาน</td>
+              <td className="text-center w-60 ">สถานะ</td>
+            </th>
+          </tr>
+          {/* </thead> */}
 
           <tbody className="bg-white">
             {data.map((item, index) => {
@@ -71,16 +71,62 @@ function EmployeeDetail() {
                   key={index}
                 >
                   <AccordionItem value="item-1">
-                    <AccordionTrigger className=" flex justify-evenly">
+                    <AccordionTrigger className="flex justify-evenly text-sm">
                       <td className="px-6 w-60 text-center">{item.order_id}</td>
                       <td className="px-6 w-60 text-center">
                         {item.order_detail.working_time}
                       </td>
-                      <td className="px-6 w-60 text-center">
+                      <td className="px-6 w-60 text-center ">
                         {item.status.status}
                       </td>
                     </AccordionTrigger>
-                    <AccordionContent className="w-full h-fit bg-slate-600"></AccordionContent>
+                    <AccordionContent>
+                      <div className="w-full h-fit bg-white flex flex-row justify-evenly">
+                        <div id="name" className="p-4">
+                          <div className="p3 text-gray-500">ชื่อ - นามสกุล</div>
+                          <div className="p2">{item.user.name}</div>
+                        </div>
+                        <div id="Tel" className="p-4">
+                          <div className="p3 text-gray-500">เบอร์โทร</div>
+                          <div className="p2">{item.user.phone}</div>
+                        </div>
+                        <div id="Email" className="p-4">
+                          <div className="p3 text-gray-500">อีเมล</div>
+                          <div className="p2">{item.user.email}</div>
+                        </div>
+                      </div>
+                      <div className="w-full h-fit bg-white flex flex-col items-start px-[125px]">
+                        <div id="Detail" className="p-4">
+                          <div className="p3 text-gray-500">รายการ</div>
+
+                          {item.service_order.map((service, index) => {
+                            return (
+                              <div className="p2" key={index}>
+                                <p>
+                                  {service.sub_service.sub_service_name}{" "}
+                                  {service.amount} {service.sub_service.unit}
+                                </p>
+                              </div>
+                            );
+                          })}
+                        </div>
+                        <div id="Place" className="p-4">
+                          <div className="p3 text-gray-500">สถานที่</div>
+                          <div className="p2">
+                            {item.order_detail.address}{" "}
+                            {item.order_detail.subdistrict}{" "}
+                            {item.order_detail.district}{" "}
+                            {item.order_detail.province}
+                          </div>
+                        </div>
+                        <div id="Etc" className="p-4">
+                          <div className="p3 text-gray-500">
+                            ข้อมูลเพิ่มเติม
+                          </div>
+                          <div className="p2">{item.order_detail.details}</div>
+                        </div>
+                      </div>
+                    </AccordionContent>
                   </AccordionItem>
                 </Accordion>
               );
