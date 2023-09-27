78926fb7c0c955896eddc2b4f3bc2dfe99eab512
themeyv <themeyv@gmail.com>
2023-09-27 17:13:28 +0700

    feat: add employee status

--- src/components/Employee/EmployeeTopbarSearch.tsx
+++ src/components/Employee/EmployeeTopbarSearch.tsx
@@ -1,86 +1,19 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { TopbarType } from "./TopbarType";
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import UserIcon from "../../assets/icon/user_icon.svg";
 
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useProduct } from "@/contexts/productsContext";
-// import useCategory from "@/hooks/useCategory";
-import useProcessing from "@/hooks/useProcessing";
-
-import search from "../../assets/icon/search.png";
-// import add from "../../assets/icon/add_symbo.png";
-import useService from "@/hooks/useService";
-// import usePromotion from "@/hooks/usePromotion";
-
-function EmployeeTopbar_search(prop: TopbarType) {
-  const navigate = useNavigate();
-  //   const { getCategory } = useCategory();
-  const { getServices } = useService();
-  //   const { getPromotions } = usePromotion();
-  const { searchCategory, searchService, searchPromotion }: any = useProduct();
-
-  const { processing }: any = useProcessing();
-  const processingObj = processing(prop.title);
-
-  useEffect(() => {
-    // if (prop.title === "หมวดหมู่") {
-    //   getCategory(searchCategory);
-    // } else
-    if (prop.title === "บริการ") {
-      getServices(searchService);
-    }
-    // else if (prop.title === "Promotion Code") {
-    //   getPromotions(searchPromotion);
-    // }
-  }, [
-    // searchCategory,
-    searchService,
-    // searchPromotion
-  ]);
-
+function EmployeeTopbar_search() {
   return (
     <nav className="flex items-center justify-between h-20 bg-white px-10 sticky top-0">
-      <h3 className="text-black text-xl font-medium">รายการ</h3>
-      <form className="flex items-center gap-6">
-        <div className="relative text-gray-500 text-base font-light">
-          <img src={search} alt="search" className="absolute left-3 top-2.5" />
-          <Input
-            type="text"
-            id="search"
-            value={processingObj.search}
-            placeholder={`ค้นหา${prop.title}...`}
-            maxLength={20}
-            className="pl-11"
-            onChange={(e) => processingObj.setSearch(e.target.value)}
-          />
-        </div>
-        {/* {prop.title === "Promotion Code" ? (
-          <Button
-            className="h-11 py-2.5 px-6 gap-2"
-            onClick={() => {
-              navigate(prop.path);
-            }}
-          >
-            เพิ่ม{` Promotion Code`}{" "}
-            <span>
-              <img src={add} alt="add" />
-            </span>
-          </Button>
-        ) : (
-          <Button
-            className="h-11 py-2.5 px-6 gap-2"
-            onClick={() => {
-              navigate(prop.path);
-            }}
-          >
-            เพิ่ม{`${prop.title}`}{" "}
-            <span>
-              <img src={add} alt="add" />
-            </span>
-          </Button>
-        )} */}
-      </form>
+      <div id="Title">
+        <h3 className="text-black text-xl font-medium">รายการ</h3>
+      </div>
+      <div id="Avatar">
+        <Avatar className="flex flex-row gap-2">
+          <AvatarImage src={UserIcon} />
+          <p>นายช่าง ไม่รู้อะไรบ้างเลย</p>
+          <AvatarFallback></AvatarFallback>
+        </Avatar>
+      </div>
     </nav>
   );
 }
